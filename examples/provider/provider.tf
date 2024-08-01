4df325b329ea1a6f98b920c3201d32fd2f52b9d3
Nguyễn Duy Quang 87 <quangnd87@fpt.com>
2024-08-02 01:07:06 +0700

    update file floating ip

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -15,26 +15,34 @@ provider "fptcloud" {
 
 
 
-data "fptcloud_instance_group_policy" "data" {
-  vpc_id = "6daffc98-fc17-4e5d-aa5d-a221517785f6"
-  filter {
-    key = "name"
-    values = ["Soft Affinity"]
-  }
-}
+# ==============================================================
+# ==============================================================
+# ==============================================================
 
-output "show_value" {
-  value = data.fptcloud_instance_group_policy.data
-}
+# data "fptcloud_instance_group_policy" "data" {
+#   vpc_id = "6daffc98-fc17-4e5d-aa5d-a221517785f6"
+#   filter {
+#     key = "name"
+#     values = ["Soft Affinity"]
+#   }
+# }
+#
+# output "show_value" {
+#   value = data.fptcloud_instance_group_policy.data
+# }
 
-
+# ==============================================================
+# ==============================================================
+# ==============================================================
 
 # resource "fptcloud_ssh_key" "example" {
 #   name = "your_ssh_name"
 #   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLAWx447unnmJLgdT0U3mu6luLioI6DI7/1zXBYT+9VAgqiBcff4kfnLLNt1k2dIO6DlzpWMgVDpXbAwr+UWGnavLrw2+2du4EQE3HQPajChuJY3bV3U6CNOzsnFdTZjwPEqifhIOQTm407wIkutZcQ8Jc/RqiB6+tA5scXdbvOOoG+wjapoz3eqw07OYgJGRgZ987LGpn1jcyHxspoE4XbYiFjRDBcQlF5bMSMsMTdUfcmG2VToSXeMgN3aeCAC+r9PcHbtGfphOsMIKMe7lda/hOepsS3Py69QzWkVOn+w/k0ZIU2chAdQo8T49Ce3PnVRpYSrxbq+X8rEKNC+aB"
 # }
 
-
+# ==============================================================
+# ==============================================================
+# ==============================================================
 
 # data "fptcloud_storage" "example" {
 #   vpc_id = "120bd194-7031-42ae-86a6-8b53a90ff9ae"
@@ -45,7 +53,9 @@ output "show_value" {
 #   value = data.fptcloud_storage.example
 # }
 
-
+# ==============================================================
+# ==============================================================
+# ==============================================================
 
 # data "fptcloud_storage_policy" "example" {
 #   vpc_id = "120bd194-7031-42ae-86a6-8b53a90ff9ae"
---
+++ fptcloud/floating-ip/datasource_floating_ip.go
@@ -0,0 +1,1 @@
+package floating_ip
