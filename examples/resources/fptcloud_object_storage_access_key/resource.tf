b4e3348f949ab436df0ee8b7118a55faad168108
hoanglm32 <hoanglm32@fpt.com>
2025-10-21 14:31:55 +0700

    docs: update example configurations for object storage resources
    - Update fptcloud_object_storage_access_key example
    - Update fptcloud_object_storage_bucket_lifecycle example
    - Ensure examples reflect current resource schemas and configurations

--- examples/resources/fptcloud_object_storage_access_key/resource.tf
+++ examples/resources/fptcloud_object_storage_access_key/resource.tf
@@ -1,4 +1,8 @@
-resource "fptcloud_object_storage_access_key" "test_key" {
+resource "fptcloud_object_storage_access_key" "create_key" {
   vpc_id      = "your_vpc_id"
-  region_name = "your_region_name"
+  region_name = "HN-02"
+}
+
+output "access_key_output" {
+  value = fptcloud_object_storage_access_key.create_key
 }
--- examples/resources/fptcloud_object_storage_bucket_lifecycle/resource.tf
+++ examples/resources/fptcloud_object_storage_bucket_lifecycle/resource.tf
@@ -5,6 +5,23 @@ resource "fptcloud_object_storage_bucket_lifecycle" "example_bucket_lifecycle" {
 
   # Option 1: Load policy from file
   life_cycle_rule_file = file("${path.module}/your_bucket_lifecycle.json")
+
+  # Option 2: Inline policy
+  # life_cycle_rule = jsonencode({
+  #     "ID": "FCI",
+  #     "Filter": {
+  #         "Prefix": ""
+  #     },
+  #     "Expiration": {
+  #         "Days": 98
+  #     },
+  #     "NoncurrentVersionExpiration": {
+  #         "NoncurrentDays": 83
+  #     },
+  #     "AbortIncompleteMultipartUpload": {
+  #         "DaysAfterInitiation": 68
+  #     }
+  # })
 }
 
 output "bucket_lifecycle" {
