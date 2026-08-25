5b9452168a38074aa2e7b5a777b0d644ab777304
minhnt1409 <minh.nt200409@gmail.com>
2026-08-26 01:35:01 +0700

    Update tag_ids dbaas

--- fptcloud/database/resource_database.go
+++ fptcloud/database/resource_database.go
@@ -345,7 +345,18 @@ func (r *resourceDatabase) Update(ctx context.Context, request resource.UpdateRe
 	plan.TimeMaintenance = state.TimeMaintenance
 
 	plan.Id = state.Id
-	response.State.Set(ctx, &plan)
+	plan.Nodes = state.Nodes
+	if plan.Nodes.IsNull() || plan.Nodes.IsUnknown() {
+		refreshed := databaseResourceModel{VpcId: state.VpcId}
+		if err := r.internalRead(ctx, state.Id.ValueString(), &refreshed); err != nil {
+			tflog.Warn(ctx, "Could not read nodes back after applying tags: "+err.Error())
+			plan.Nodes = emptyNodesList()
+		} else {
+			plan.Nodes = refreshed.Nodes
+		}
+	}
+
+	response.Diagnostics.Append(response.State.Set(ctx, &plan)...)
 }
 
 func (r *resourceDatabase) Delete(ctx context.Context, request resource.DeleteRequest, response *resource.DeleteResponse) {
@@ -531,9 +542,10 @@ func (r *resourceDatabase) Schema(ctx context.Context, request resource.SchemaRe
 				Description: "List of tag IDs applied to the database",
 			},
 			"nodes": schema.ListAttribute{
-				Computed:    true,
-				Description: "List of nodes (VMs) in the database cluster",
-				ElementType: types.ObjectType{
+				Computed:    		true,
+				PlanModifiers:	[]planmodifier.List{listplanmodifier.UseStateForUnknown()},
+				Description: 		"List of nodes (VMs) in the database cluster",
+				ElementType: 		types.ObjectType{
 					AttrTypes: map[string]attr.Type{
 						"id":             types.StringType,
 						"name":           types.StringType,
